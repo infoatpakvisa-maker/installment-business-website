49168d8d335313a260d9023ffce8f07ec2203da9
najibullah-platforms <najibullah.platforms@gmail.com>
2026-08-02 10:08:09 +0500

    map is set on contact page

--- src/components/GoogleMap/GoogleMap.jsx
+++ src/components/GoogleMap/GoogleMap.jsx
@@ -1,14 +1,15 @@
 function GoogleMap() {
   return (
     <iframe
-      title="Google Map"
-      src="YOUR_GOOGLE_MAP_EMBED_LINK"
+      title="Fazal Electronics Location"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d197.9894601765659!2d73.0275861246422!3d33.58904955079615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xb699cdad57ca5d1%3A0xf394800cb3a3c2d0!2sfazal%20electronics!5e1!3m2!1sen!2s!4v1785647204836!5m2!1sen!2s"
       width="100%"
       height="450"
       style={{ border: 0 }}
-      loading="lazy"
       allowFullScreen
-    ></iframe>
+      loading="lazy"
+      referrerPolicy="strict-origin-when-cross-origin"
+    />
   );
 }
 
